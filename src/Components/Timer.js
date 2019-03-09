19a4e62458f3ecc0febc808b34fa245abae42dfb
antonwy <anton.wyrowski@gmail.com>
2019-03-09 14:29:27 +0100

    added multiple timer

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -116,7 +116,7 @@ const getCurrentYear = () => {
 
 Timer.defaultProps = {
     date: `${getCurrentYear()}-05-03T00:00:00`,
-    height: "100px",
+    height: "10vw",
     width: "",
     className: "",
     style: {},
